fbc443ef5bbb8f6154d6321ec541183eb1fe3fb8
BorisvEwijk <Boris.vanewijk@edu-kw1c.nl>
2020-02-17 14:43:29 +0100

    opdracht 2.1 en 2.2 af

--- Opdracht2.2.php
+++ Opdracht2.2.php
@@ -6,12 +6,12 @@ $text3 = "toch zo'n makkelijke taal";
 $text4 = "wat is";
 $text5 = "PHP";
 $text6 = "Nooit gedacht dat";
-$text7 = "de installatie is best ingewikkeld";
-$text8 = "fijn";
+$text7 = "De installatie is best ingewikkeld";
+$text8 = "Fijn";
 $text9 = "?";
 $text10 = ".";
 $text11 = ",";
-//$text12 = <br>;
+$text12 = "<br>";
 $text13 = "is";
 $text14 = "Vind je niet";
 $text15 = "toch";
@@ -19,11 +19,56 @@ $text15 = "toch";
 <p>
     <?php
     echo $text1;
+    echo " ";
     echo $text4;
+    echo " ";
     echo $text5;
+    echo " ";
     echo $text3;
+    echo " ";
     echo $text10;
-  //  echo $text12;
-?>
+    echo $text12;
+    echo $text7;
+    echo $text10;
+    echo " ";
+    echo $text8;
+    echo " ";
+    echo $text15;
+    echo $text9;
+    echo $text12;
+    echo $text6;
+    echo " ";
+    echo $text5;
+    echo " ";
+    echo $text3;
+    echo " ";
+    echo $text13;
+    echo $text10;
+    ?>
+</p>
+<p>
+    <?php
+    echo $text1;
+    echo $text11;
+    echo $text12;
+    echo $text8;
+    echo " ";
+    echo $text15;
+    echo " ";
+    echo "dat";
+    echo $text5;
+    echo " ";
+    echo "zo'n makkelijke taal";
+    echo " ";
+    echo $text13;
+    echo $text10;
+    echo $text12;
+    echo $text7;
+    echo $text10;
+    echo " ";
+    echo $text14;
+    echo $text9;
+
+    ?>
 </p>
 
