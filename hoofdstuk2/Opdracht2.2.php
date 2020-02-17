7a1d72629a2aa92c1d91bfdb5f73f2bf00dc3949
BorisvEwijk <Boris.vanewijk@edu-kw1c.nl>
2020-02-17 15:00:08 +0100

    Correctie 2.2

--- hoofdstuk2/Opdracht2.2.php
+++ hoofdstuk2/Opdracht2.2.php
@@ -6,75 +6,94 @@
  * File: Opdracht2.2.php
  */
 
-$text1 = "Hallo";
-$text2 = "een makkelijke taal";
-$text3 = "toch zo'n makkelijke taal";
-$text4 = "wat is";
-$text5 = "PHP";
-$text6 = "Nooit gedacht dat";
-$text7 = "De installatie is best ingewikkeld";
-$text8 = "Fijn";
-$text9 = "?";
-$text10 = ".";
-$text11 = ",";
-$text12 = "<br>";
-$text13 = "is";
-$text14 = "Vind je niet";
-$text15 = "toch";
 ?>
-<p>
-    <?php
-    echo $text1;
-    echo " ";
-    echo $text4;
-    echo " ";
-    echo $text5;
-    echo " ";
-    echo $text3;
-    echo " ";
-    echo $text10;
-    echo $text12;
-    echo $text7;
-    echo $text10;
-    echo " ";
-    echo $text8;
-    echo " ";
-    echo $text15;
-    echo $text9;
-    echo $text12;
-    echo $text6;
-    echo " ";
-    echo $text5;
-    echo " ";
-    echo $text3;
-    echo " ";
-    echo $text13;
-    echo $text10;
-    ?>
-</p>
-<p>
-    <?php
-    echo $text1;
-    echo $text11;
-    echo $text12;
-    echo $text8;
-    echo " ";
-    echo $text15;
-    echo " ";
-    echo "dat";
-    echo $text5;
-    echo " ";
-    echo "zo'n makkelijke taal";
-    echo " ";
-    echo $text13;
-    echo $text10;
-    echo $text12;
-    echo $text7;
-    echo $text10;
-    echo " ";
-    echo $text14;
-    echo $text9;
-
-    ?>
 </p>
 
+<!doctype html>
+<html>
+<head>
+    <title>Index opdrachten</title>
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2><a href="../index.php">Terug</a></h2>
+</aside>
+<main>
+    <?php
+    $text1 = "Hallo";
+    $text2 = "een makkelijke taal";
+    $text3 = "toch zo'n makkelijke taal";
+    $text4 = "wat is";
+    $text5 = "PHP";
+    $text6 = "Nooit gedacht dat";
+    $text7 = "De installatie is best ingewikkeld";
+    $text8 = "Fijn";
+    $text9 = "?";
+    $text10 = ".";
+    $text11 = ",";
+    $text12 = "<br>";
+    $text13 = "is";
+    $text14 = "Vind je niet";
+    $text15 = "toch";
+    ?>
+    <p>
+        <?php
+        echo $text1;
+        echo " ";
+        echo $text4;
+        echo " ";
+        echo $text5;
+        echo " ";
+        echo $text3;
+        echo " ";
+        echo $text10;
+        echo $text12;
+        echo $text7;
+        echo $text10;
+        echo " ";
+        echo $text8;
+        echo " ";
+        echo $text15;
+        echo $text9;
+        echo $text12;
+        echo $text6;
+        echo " ";
+        echo $text5;
+        echo " ";
+        echo $text3;
+        echo " ";
+        echo $text13;
+        echo $text10;
+        ?>
+    </p>
+    <p>
+        <?php
+        echo $text1;
+        echo $text11;
+        echo $text12;
+        echo $text8;
+        echo " ";
+        echo $text15;
+        echo " ";
+        echo "dat";
+        echo " ";
+        echo $text5;
+        echo " ";
+        echo "zo'n makkelijke taal";
+        echo " ";
+        echo $text13;
+        echo $text10;
+        echo $text12;
+        echo $text7;
+        echo $text10;
+        echo " ";
+        echo $text14;
+        echo $text9;
+
+        ?>
+</main>
+</body>
+</html>
