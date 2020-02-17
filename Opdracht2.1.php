9b0b85d407c5adb962a8c0827511ba7db97e48c8
BorisvEwijk <Boris.vanewijk@edu-kw1c.nl>
2020-02-17 14:06:21 +0100

    opdracht 21 af

---
+++ Opdracht2.1.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: Boris van Ewijk
+ * Date: 17-2-2020
+ * Time: 13:34
+ * File: Opdracht2.1.php
+ */
+
+
+
+echo "<p>Hello World<p>"
+
+?>
+
+
---
+++ Opdracht2.2.php
@@ -0,0 +1,29 @@
+<?php
+
+$text1 = "Hallo";
+$text2 = "een makkelijke taal";
+$text3 = "toch zo'n makkelijke taal";
+$text4 = "wat is";
+$text5 = "PHP";
+$text6 = "Nooit gedacht dat";
+$text7 = "de installatie is best ingewikkeld";
+$text8 = "fijn";
+$text9 = "?";
+$text10 = ".";
+$text11 = ",";
+//$text12 = <br>;
+$text13 = "is";
+$text14 = "Vind je niet";
+$text15 = "toch";
+?>
+<p>
+    <?php
+    echo $text1;
+    echo $text4;
+    echo $text5;
+    echo $text3;
+    echo $text10;
+  //  echo $text12;
+?>
+</p>
+
